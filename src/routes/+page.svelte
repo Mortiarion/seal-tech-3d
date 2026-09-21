06fdd343550546d36aa84ab254e7f927366ad8a4
Mortiarion <andre-endju@ukr.net>
2026-09-21 03:21:46 +0300

    1. думаю це остаточний варіант для нашої не великої студії ваших мрій і ідей

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Hero from '$lib/components/landing/sections/Hero.svelte';
 	import About from '$lib/components/landing/sections/About.svelte';
-	import Services from '$lib/components/landing/sections/Services.svelte';
 	import Portfolio from '$lib/components/landing/sections/Portfolio.svelte';
 	import Contact from '$lib/components/landing/sections/Contacts.svelte';
 	import Procces from '$lib/components/landing/sections/Procces.svelte';
@@ -34,8 +33,6 @@
 <About />
 
 <Procces />
-
-<Services />
 
 <Portfolio />
 
